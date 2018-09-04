b9e2fa5dca6f8dd7880e71e7c54cc2ecafb54ced
Danijel Jovanović <36568690+DanijelXDA@users.noreply.github.com>
2018-09-04 21:38:57 +0200

    Create zvezdice.cs

---
+++ Uvod/zvezdice.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class MainClass 
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(" *  * *  *");
+        Console.WriteLine("*    *    *");
+        Console.WriteLine("*         *");
+        Console.WriteLine("*         *");
+        Console.WriteLine("*         *");
+        Console.WriteLine(" *       *");
+        Console.WriteLine("  *     *");
+        Console.WriteLine("   *   *");
+        Console.WriteLine("     *");
+ }
+} 
