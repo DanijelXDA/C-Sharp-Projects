ad8c8431b006b9e93f52584024f3bc520fa1a2dd
Danijel Jovanović <36568690+DanijelXDA@users.noreply.github.com>
2018-09-04 21:40:59 +0200

    Create račun.cs

---
+++ Uvod/račun.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class MainClass 
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(" 11");
+        Console.WriteLine(" 31");
+        Console.WriteLine(" 58");
+        Console.WriteLine("+23");
+        Console.WriteLine("===");
+        Console.WriteLine("123");
+ }
+} 
