01559145b78bec650dc63bd461096b06a8ba082b
Danijel Jovanović <36568690+DanijelXDA@users.noreply.github.com>
2018-09-04 21:28:23 +0200

    Create vezbanje-ispisa.cs

---
+++ Uvod/vezbanje-ispisa.cs
@@ -0,0 +1,13 @@
+using System;
+
+class MainClass 
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ime: Danijel");
+        Console.WriteLine("Prezime: Jovanović");
+        Console.WriteLine("Nadimak: Dani");
+        Console.WriteLine("Adresa: Ulica Bubble Bee, broj 314, 11000 Beograd");
+        Console.WriteLine("Mobilni: 064/314");
+    }
+}
