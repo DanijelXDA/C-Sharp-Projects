73d656f3d1d5f6178f458146a4374bd93b4e6587
Danijel Jovanović <36568690+DanijelXDA@users.noreply.github.com>
2018-09-08 17:13:58 +0200

    Create definicija-promenljive.cs

---
+++ Uvod/definicija-promenljive.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class MainClass 
+{
+    public static void Main(string[] args)
+    {
+        int x =int.Parse(Console.ReadLine()); // Konvertovanje stringa u int
+        Console.WriteLine(x); // Ispis na konzolu
+ }
+}
