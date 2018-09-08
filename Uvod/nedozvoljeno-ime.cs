2f151c80adaf93e0372eb15e789e95ccf5196b96
Danijel Jovanović <36568690+DanijelXDA@users.noreply.github.com>
2018-09-08 17:16:54 +0200

    Create nedozvoljeno-ime.cs

---
+++ Uvod/nedozvoljeno-ime.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class MainClass 
+{
+    public static void Main(string[] args)
+    {
+        int ia = 1;
+        int ib = 2;
+        int ic = 3;
+        int id = 4;
+        int ie = 5;
+        // Ne moze biti rezervisana reč
+        // int if = 6; 
+        int lf = 6;
+
+        Console.WriteLine(ia+ib+ic+id+ie+lf); // Ispis na konzolu
+ }
+}
