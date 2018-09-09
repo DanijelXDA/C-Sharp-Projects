730c2f3b39fb9c178e6a12f59cc35fb6136d3f1a
Danijel Jovanović <36568690+DanijelXDA@users.noreply.github.com>
2018-09-09 12:56:06 +0200

    Create konverzija-vremena.cs

---
+++ Uvod/konverzija-vremena.cs
@@ -0,0 +1,33 @@
+using System;
+
+class MainClass 
+  {
+    public static void Main (string[] args) 
+      {
+        int sekunde = int.Parse(Console.ReadLine());
+
+        int hh, mm, ss;
+
+        hh = sekunde / 3600;
+        mm = ( sekunde - (hh * 3600) ) / 60;
+        ss = sekunde -  ( mm * 60 ) + ( hh * 3600 );
+
+        if( hh > 24 ) // Provera da li smo premašili opseg sati
+          hh -= 24; 
+
+        if( mm > 60)
+         {
+           hh++;
+           mm -= 60;
+         }
+
+        if( ss > 60 )
+        {
+          mm++;
+          ss -= 60;
+        }
+
+        Console.WriteLine("{0}:{1}:{2}", hh, mm, ss);
+        
+      }
+}
