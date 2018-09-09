ea06af470101e231f5fd9797cdb360e3a89c6ec3
Danijel Jovanović <36568690+DanijelXDA@users.noreply.github.com>
2018-09-09 12:47:42 +0200

    Create unos-ispis-broja.cs

---
+++ Uvod/unos-ispis-broja.cs
@@ -0,0 +1,12 @@
+using System;
+
+class MainClass 
+  {
+    public static void Main (string[] args) 
+      {
+        int prviBroj = int.Parse(Console.ReadLine());
+        int drugiBroj = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("{0} {1}", prviBroj, drugiBroj);
+      }
+}
