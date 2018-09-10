5319cf069a15807d192969d7cf1aedfebd8dd6d6
Danijel Jovanović <36568690+DanijelXDA@users.noreply.github.com>
2018-09-10 22:39:39 +0200

    Change algorithm -> Code works

--- Uvod/poslednji-skolski-cas.cs
+++ Uvod/poslednji-skolski-cas.cs
@@ -1,33 +1,25 @@
 using System;
 
-public class MainClass
+class MainClass 
 {
-    public static void Main(string[] args)
+    public static void Main (string[] args) 
     {
-        int brojCasova, i, minuti = 0;
+        int N;
 
-        Console.WriteLine("Unesi koliko casova imas danas: ");
-        brojCasova = int.Parse(Console.ReadLine());
+        N = int.Parse(Console.ReadLine());
 
-
-        minuti += brojCasova * 45;
-        for (i = 2; i < brojCasova; ++i)
+        switch(N) 
         {
-            minuti += 5;
-        }
+          case 1 : Console.WriteLine("8:45"); break;
+          case 2 : Console.WriteLine("9:35"); break;
+          case 3 : Console.WriteLine("10:40"); break;
+          case 4 : Console.WriteLine("11:30"); break;
+          case 5 : Console.WriteLine("12:20"); break;
+          case 6 : Console.WriteLine("13:10"); break;
+          case 7 : Console.WriteLine("14:00"); break;
+          case 8 : Console.WriteLine("14:50"); break;
+          case 9 : Console.WriteLine("15:30"); break;
+      }
 
-        // i je lokalnog nivoa!
-        if ( i > 2)
-             minuti += 20;
-
-
-        int sati = 8; // 8 ujutru
-
-        sati += minuti / 60;
-        minuti -= (sati - 8) * 60; // Pretvaranje u minute
-
-        Console.WriteLine(sati + ":" + minuti); // Ispis
-
-        Console.ReadKey();
     }
 }
