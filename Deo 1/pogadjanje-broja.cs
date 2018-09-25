93e4d05e860a2999bf1a3a3c4acb6dbe6376913b
Danijel Jovanović <36568690+DanijelXDA@users.noreply.github.com>
2018-09-25 11:31:18 +0200

    Create pogadjanje-broja.cs

---
+++ Deo 1/pogadjanje-broja.cs
@@ -0,0 +1,48 @@
+/*
+ * C# Program koji generiše broj koji korisnik pogađa
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            int zamisljeniBroj = slucajanBroj(1, 101);
+            int pokusaji = 1;
+            while (true)
+            {
+                Console.Write("\nUnesite broj u opsegu od 1 do 100 (0 za kraj programa): ");
+                int ulaz = Convert.ToInt32(Console.ReadLine());
+                if (ulaz == 0)
+                    return;
+                else if (ulaz < zamisljeniBroj)
+                {
+                    Console.WriteLine("Premali broj, pokušaj ponovo.");
+                    ++pokusaji;
+                    continue;
+                }
+                else if (ulaz > zamisljeniBroj)
+                {
+                    Console.WriteLine("Preveliki broj, pokušaj ponovo.");
+                    ++pokusaji;
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("Pogodili ste! Broj koji sam zamislio je {0}!", zamisljeniBroj);
+                    Console.WriteLine("Uspeli ste iz {0}. pokušaja.\n", pokusaji);
+                    break;
+                }
+            }
+        }
+
+    }
+    static int slucajanBroj(int min, int max)
+    {
+        Random random = new Random();
+        return random.Next(min, max);
+    }
+}
