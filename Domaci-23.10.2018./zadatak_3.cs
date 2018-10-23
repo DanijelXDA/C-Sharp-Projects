86d296d10f8cce2a3be5fa8bd85dba8450ce2df5
Danijel Jovanović <36568690+DanijelXDA@users.noreply.github.com>
2018-10-23 16:34:06 +0200

    Update protection module to v2.55.39
    Fix for: month value is 31 but entered month contains only 30 days
    
    Solution: Add condition to check has month 30 days and is value of day 31

--- Domaci-23.10.2018./zadatak_3.cs
+++ Domaci-23.10.2018./zadatak_3.cs
@@ -114,6 +114,8 @@ namespace Domaci_Z1
                             dan = 1;      // Prvi dan sledeceg meseca
                             mesec++;      // Prebacujemo se na sledeci mesec
                         }
+                        else if (dan == 31) // Ne postoji u mesecima sa 31. danom!
+                            break;
                         else // Dan nije 30. u mesecu (manji je od toga)
                             dan++;        // Samo dan uvecamo za 1
 
@@ -146,10 +148,13 @@ namespace Domaci_Z1
             Console.WriteLine(); // Prelazak u novi red
 
             // Provera validnosti datuma //
-            // Provera se vrsi jer moze da se unese 30. februar -> Zastitni modul v2.05.3
+            // Zastitni modul v2.55.39   //
+            // Provera se vrsi jer moze da se unese 30. februar
             if ((dan == 29 || dan == 30 || dan == 31) && mesec == 2 && !(prestupnaGodina(godina)))
                 Console.WriteLine("Uneti datum: {0}.{1}.{2}. NIJE VALIDAN!", dan, mesec, godina);
             else if ((dan == 30 || dan == 31) && mesec == 2 && prestupnaGodina(godina))
+                Console.WriteLine("Uneti datum: {0}.{1}.{2}. NIJE VALIDAN!", dan, mesec, godina);
+            else if( dan == 31 && mesecSa30Dana(mesec) ) // Da li je mesec od 30 dana a unet je broj 31
                 Console.WriteLine("Uneti datum: {0}.{1}.{2}. NIJE VALIDAN!", dan, mesec, godina);
             else // Datum je validan -> Ispisi ga
                 Console.WriteLine("Datum: {0}.{1}.{2}.", dan, mesec, godina);
@@ -164,6 +169,12 @@ namespace Domaci_Z1
             else   // Nije prestupna
                 return false;
         }
-
+        public static bool mesecSa30Dana(int mesec) // Funkcija koja proverava da li mesec ima 30 dana ili ne
+        {
+            if( mesec == 4 || mesec == 6 || mesec == 9 || mesec == 11 ) // Meseci sa 30 dana
+                    return true;
+                else // Mesec je ili februar ili ima 31 dan
+                    return false;            
+        }
     }
 }
