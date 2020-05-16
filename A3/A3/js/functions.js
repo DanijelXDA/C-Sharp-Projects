85ec4567307607758081b3fed6f559f470194ad5
Danijel Jovanović <danijel.xda@gmail.com>
2020-05-16 21:34:49 +0200

    A3 - Urađen

---
+++ A3/A3/js/functions.js
@@ -0,0 +1,5 @@
+﻿function fun() {
+    var x = document.getElementById("selectgrad").value;
+    var url = "https://naslovi.net/vremenska-prognoza/" + x;
+    document.getElementById("prognoza").src = url;
+}
