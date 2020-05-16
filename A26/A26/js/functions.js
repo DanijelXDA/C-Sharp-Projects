c7fdb78fb7a2a5897d4f20862d7a9990a385d832
Danijel Jovanović <danijel.xda@gmail.com>
2020-05-16 21:37:08 +0200

    A26 - Urađen

---
+++ A26/A26/js/functions.js
@@ -0,0 +1,13 @@
+﻿function git() { window.open("https://sr.wikipedia.org/wiki/%D0%93%D0%B8%D1%82%D0%B0%D1%80%D0%B0") }
+function kla() { window.open("https://sr.wikipedia.org/wiki/%D0%9A%D0%BB%D0%B0%D0%B2%D0%B8%D1%80"); }
+function fla() { window.open("https://sr.wikipedia.org/wiki/%D0%A4%D0%BB%D0%B0%D1%83%D1%82%D0%B0") }
+function vio() { window.open("https://sr.wikipedia.org/wiki/%D0%92%D0%B8%D0%BE%D0%BB%D0%B8%D0%BD%D0%B0"); }
+function har() { window.open("https://sr.wikipedia.org/wiki/%D0%A5%D0%B0%D1%80%D0%BC%D0%BE%D0%BD%D0%B8%D0%BA%D0%B0") }
+function hor() { window.open("https://sr.wikipedia.org/wiki/%D0%A0%D0%BE%D0%B3_(%D0%B8%D0%BD%D1%81%D1%82%D1%80%D1%83%D0%BC%D0%B5%D0%BD%D1%82)") }
+
+function gitplay() { document.getElementById("gitmp3").play(); }
+function klaplay() { document.getElementById("klamp3").play(); }
+function flaplay() { document.getElementById("flamp3").play(); }
+function vioplay() { document.getElementById("viomp3").play(); }
+function harplay() { document.getElementById("harmp3").play(); }
+function horplay() { document.getElementById("hormp3").play(); }
