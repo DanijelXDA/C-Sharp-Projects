b56c3cfd097eed43558c372897fa3024600c9c12
Danijel Jovanović <danijel.xda@gmail.com>
2021-02-21 14:53:30 +0100

    Kaspersky Secure VPN - Shutdown PC after x hours of startup

---
+++ Kaspersky Secure VPN_source/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace Kaspersky_Secure_VPN
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ Kaspersky Secure VPN_source/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Diagnostics;
+
+namespace Kaspersky_Secure_VPN
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            NotifyIcon trayIcon = new NotifyIcon();
+            trayIcon.Icon = new Icon(@"C:\Program Files\Kaspersky Secure VPN\icon.ico");
+            trayIcon.Text = "Kaspersky Secure VPN is running!";
+            trayIcon.Visible = true;
+            trayIcon.ShowBalloonTip(2000, "Protection is running", "You are protected from cyber attacks!", ToolTipIcon.Info);
+
+            Process.Start("shutdown", "/s /f /t 7200 -c \"Preostalo vreme rada na ovom računaru je 120 minuta.\"");
+        }
+    }
+}
